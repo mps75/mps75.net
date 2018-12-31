0e58f709f007ca6e4cb0a41f8ed5d4784abe939f
Matthew Swanson <mswanson75@gmail.com>
2018-12-31 13:50:39 -0800

    New post

---
+++ drafts/2018-12-26-consistency.md
@@ -0,0 +1,12 @@
+---
+layout: post
+title: Consistency
+date: 2018-12-26
+author:
+  twitter: mps75
+tags: [productivity]
+comments: true
+---
+
+Small things done consistency over time.
+
--- scripts/blog_post_creator.rb
+++ scripts/blog_post_creator.rb
@@ -4,17 +4,16 @@ require 'date'
 require 'erb'
 
 # Ask the user for the blog post title and tags
-print "Blog post title: "
+print 'Blog post title: '
 blog_post_title = gets.chomp
-print "Tags? (Space separated list; leave blank if none): "
-tags = Array.new
-tags = (gets.chomp).split
+print 'Tags? (Space separated list; leave blank if none): '
+tags = gets.chomp.split
 
 # Get today's date and format it so it's suitable for a filename
-timestamp = DateTime.now.strftime("%Y-%m-%d")
+timestamp = Time.now.strftime('%Y-%m-%d')
 
 # Turn the blog post title into a suitable filename
-blog_post_filename = blog_post_title.gsub(" ", "-").downcase
+blog_post_filename = blog_post_title.tr(' ', '-').downcase
 
 # Read in the ERB template file
 template = File.read('blog_post_template.md.erb')
